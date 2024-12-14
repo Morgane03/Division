9c7d61d0f5cdeae32f3afed1f43a506ba8377741
m v <morgane.voisin@e-artsup.net>
2024-12-14 16:31:07 +0100

    Jump + checkpoint + trap

---
+++ Assets/Script/General/Checkpoints.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour
+{
+    public Vector2 checkpointPosition;
+    public GameObject player;
+
+    private void Start()
+    {
+        checkpointPosition = transform.position; // Store the checkpoint position
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Save the checkpoint position in the player's script
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.SetCheckpoint(checkpointPosition);
+            }
+        }
+    }
+}
--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,12 +6,16 @@ public class PlayerMovement : MonoBehaviour
     //public event Action StartedMoving;
     //public event Action StoppedMoving;
 
+    [SerializeField]
+    private PlayerMain _playerMain;
+
     private Vector2 _moveInput;
 
     [SerializeField]
     private float moveSpeed = 5f;
     [SerializeField]
     private float jumpForce = 5f;
+    private float _gravity = -9.8f;
 
     private Rigidbody2D _rb;
 
@@ -32,13 +33,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 movement = _moveInput * moveSpeed * Time.fixedDeltaTime;
-        _rb.MovePosition(_rb.position + movement);
+        Vector2 moveVelocity = new Vector2(_moveInput.x * moveSpeed, _rb.velocity.y);
+        _rb.velocity = moveVelocity;
     }
 
     public void Jump()
     {
-        //_rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+        _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
