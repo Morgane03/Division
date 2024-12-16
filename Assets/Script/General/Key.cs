0989baa9bf6d7f3698224b3486eb7fce7f5e0b39
m v <morgane.voisin@e-artsup.net>
2024-12-16 18:58:07 +0100

    Base music loop + door open and close + door flip

--- Assets/Script/General/Key.cs
+++ Assets/Script/General/Key.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    [HideInInspector]
     public bool isCollected = false; // Indique si la clé a été collectée
+
+    [SerializeField] private GameObject _door; // Référence à l'objet de la porte
+    [SerializeField] private Sprite _doorOpen;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,6 +19,7 @@ public class Key : MonoBehaviour
 
             // Désactiver l'objet de la clé
             gameObject.SetActive(false);
+            _door.GetComponent<SpriteRenderer>().sprite = _doorOpen;
 
             // Optionnel : Vous pouvez jouer un son ou une animation ici
         }
--- Assets/Script/General/NextLevel.cs
+++ Assets/Script/General/NextLevel.cs
@@ -21,7 +21,6 @@ public class NextLevel : MonoBehaviour
     [SerializeField] private PlayerSpleetScreen _playerScreen;
 
     [SerializeField] private Sprite _doorClose;
-    [SerializeField] private Sprite _doorOpen;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -31,8 +30,6 @@ public class NextLevel : MonoBehaviour
             // Vérifiez si la clé a été collectée
             if (key.isCollected)
             {
-                _door.GetComponent<SpriteRenderer>().sprite = _doorOpen;
-
                 switch (level)
                 {
                     case 1:
@@ -44,20 +41,17 @@ public class NextLevel : MonoBehaviour
                         break;
                     case 2:
                         _playerRobot.transform.position = levels[1].transform.position;
-                        _door.GetComponent<SpriteRenderer>().flipX = true;
                         key.transform.position = _keysPosition[1].transform.position;
                         _door.transform.position = _doorsPosition[1].transform.position;
                         Next();
                         break;
                     case 3:
                         _playerRobot.transform.position = levels[2].transform.position;
-                        _door.GetComponent<SpriteRenderer>().flipX = true;
                         key.transform.position = _keysPosition[2].transform.position;
                         _door.transform.position = _doorsPosition[2].transform.position;
                         break;
                     case 4:
                         _playerRobot.transform.position = levels[3].transform.position;
-                        _door.GetComponent<SpriteRenderer>().flipX = true;
                         key.transform.position = _keysPosition[3].transform.position;
                         _door.transform.position = _doorsPosition[3].transform.position;
                         break;
@@ -82,10 +76,32 @@ public class NextLevel : MonoBehaviour
     {
         level++;
         key.isCollected = false;
-        _door.GetComponent<SpriteRenderer>().flipX = false;
+        SwitchDoorSprite();
         _door.GetComponent<SpriteRenderer>().sprite = _doorClose;
         _playerScreen.SpleetScreenOff();
         key.gameObject.SetActive(true);
+    }
+
+    private void SwitchDoorSprite()
+    {
+        switch (level)
+        {
+            case 1:
+                _door.GetComponent<SpriteRenderer>().flipX = false;
+                break;
+            case 2:
+                _door.GetComponent<SpriteRenderer>().flipX = true;
+                break;
+            case 3:
+                _door.GetComponent<SpriteRenderer>().flipX = true;
+                break;
+            case 4:
+                _door.GetComponent<SpriteRenderer>().flipX = true;
+                break;
+            default:
+                Debug.Log("Niveau non défini !");
+                break;
+        }
     }
 
     private IEnumerator DisableText()
