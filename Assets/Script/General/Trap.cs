dea98e2285eb33388331b81f3aa72743279703e2
m v <morgane.voisin@e-artsup.net>
2024-12-27 11:09:24 +0100

    Transition if player collide trap

--- Assets/Script/General/Trap.cs
+++ Assets/Script/General/Trap.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _transitionScreen;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -9,8 +12,9 @@ public class Trap : MonoBehaviour
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             if (playerController != null)
             {
+                _transitionScreen.SetActive(true);
                 UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
-                playerController.Respawn();
+                //playerController.Respawn();
             }
         }
     }
