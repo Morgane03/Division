d305f7ac1bdfb08cd3cfa0a1efc8f429c2db385c
m v <morgane.voisin@e-artsup.net>
2024-12-27 11:26:05 +0100

    add footstep human player

--- Assets/Script/Audio/PlayerSound.cs
+++ Assets/Script/Audio/PlayerSound.cs
@@ -1,18 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private List<AudioClip> _footstepClips;
 
-    // Update is called once per frame
-    void Update()
+    public void PlayFootstepSound()
     {
-        
+        _audioSource.PlayOneShot(_footstepClips[Random.Range(0, _footstepClips.Count)]);
     }
 }
